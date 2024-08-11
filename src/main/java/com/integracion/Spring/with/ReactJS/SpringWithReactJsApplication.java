66d2101b358e8259257dd30caba620ab4211548d
John <johndoe@example.com>
2024-08-11 17:46:18 -0600

    first commit

---
+++ src/main/java/com/integracion/Spring/with/ReactJS/SpringWithReactJsApplication.java
@@ -0,0 +1,13 @@
+package com.integracion.Spring.with.ReactJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithReactJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithReactJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integracion/Spring/with/ReactJS/SpringWithReactJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integracion.Spring.with.ReactJS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithReactJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
